660f766c9a5de3a474d3416360580e208c614b8d
Thalassa <jannaki69@hotmail.com>
2024-03-29 23:38:26 +0100

    added search for duties testing noofemails2

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,7 @@ const NodeMailer = require('nodemailer');
 const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
 const { application } = require("express");
 
-require("dotenv").config();
+//require("dotenv").config();
 
 var numberOfEmails = 0;
 var lastEmailTime = new Date();
@@ -27,164 +27,127 @@ const scrapeLogic = async (res) =>
   });
 
 
-  try {
-    
+  try 
+  {
     const page = await browser.newPage();
     await searchDuties(page);
-
-/*
-    await page.goto("https://developer.chrome.com/");
-
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-
-        */
-  } catch (e) {
+    await browser.close();
+  } 
+  catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
-
-
   } finally {
-//    await browser.close();
+    await browser.close();
   }
 };
 
 async function searchDuties(page){
 
-  try {
+  try 
+  {
     //const page = await browser.newPage();
     await page.goto(openDutiesText);
 
-      
-      //async () => 
-      //{
-        /*
-        var timeNow = Date.now();
-        var timeDiff = timeNow - lastEmailTime; //in ms
-        // strip the ms
-        timeDiff /= (1000*60);
+    var timeNow = Date.now();
+    var timeDiff = timeNow - lastEmailTime; //in ms
+    // strip the ms
+    timeDiff /= (1000*60);
 
-        // get seconds 
-        var minutesSinceLastEmail = Math.round(timeDiff);
+    // get seconds 
+    var minutesSinceLastEmail = Math.round(timeDiff);
 
-        if (minutesSinceLastEmail > 30)
-        {
-          //reset
-          numberOfEmails = 0;
-          lastEmailTime = Date.now();
-        }
+    if (minutesSinceLastEmail > 30)
+    {
+      //reset
+      numberOfEmails = 0;
+      lastEmailTime = Date.now();
+    }
         
-        if (numberOfEmails > 4)
+    if (numberOfEmails > 1)
+    {
+      console.log(minutesSinceLastEmail.toString() + " minutes since last email and no of emails > 4");
+    }
+    else
+    {
+  
+      await page.reload();
+      console.log("RELOAD done " + page.url().toString());
+
+      //await page.goto('file:///C:/Users/janna/VS/w3school/SISCOG Web App c2 en reservert en ledig.html');
+
+      if ('https://tpowebservice.nsb.no:8501/app/login' == page.url().toString()) 
+      {
+        await loginPage(page);
+      }
+      
+      //page.once('load', () => console.log('Page loaded!'));
+      let found=false;
+      
+      const sleep =(ms=30000) => new Promise(resolve => setTimeout(resolve, ms));
+      await sleep(10000);
+
+      const extractedText = await page.$eval('*', (el) => el.innerText);
+      //console.log("INNER TEXT:");
+      //console.log(extractedText);
+
+      //const extractedText2 = await page.$eval('*', (el) => el.innerHTML);
+      //console.log("INNER HTML:");
+      //console.log(extractedText2);
+      
+      let t = String(extractedText).toUpperCase();
+      let position1 = t.search("DUTIES AVAILABLE");
+      let position2 = t.search("Det finnes ingen ledige eller reserverte dagsverk".toUpperCase());
+      if (position2 != -1  )
         {
-          console.log(minutesSinceLastEmail.toString() + " minutes since last email and no of emails > 4");
+          found=true;
+          //console.log("Found .." + position1.toString() + "  " + position2.toString());
         }
-        else
-        */
-        {
-          
-          await page.reload();
-          console.log("RELOAD done " + page.url().toString());
 
-          //await page.goto('file:///C:/Users/janna/VS/w3school/SISCOG Web App c2 en reservert en ledig.html');
+      if (found) 
+      {
+        console.log('FOUND AVAILABLE dutie');
 
-          if ('https://tpowebservice.nsb.no:8501/app/login' == page.url().toString()) 
-          {
-            await loginPage(page);
+        var transporter = NodeMailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'jannaki69@gmail.com',
+            pass: 'qopo mxog serg xmur'
           }
-          
-          //page.once('load', () => console.log('Page loaded!'));
-          let found=false;
-          
-          const sleep =(ms=30000) => new Promise(resolve => setTimeout(resolve, ms));
-          await sleep(30000);
+        });
 
-          const extractedText = await page.$eval('*', (el) => el.innerText);
-          console.log("INNER TEXT:");
-          console.log(extractedText);
-
-          //const extractedText2 = await page.$eval('*', (el) => el.innerHTML);
-          //console.log("INNER HTML:");
-          //console.log(extractedText2);
-          
-          let t = String(extractedText).toUpperCase();
-          let position1 = t.search("DUTIES AVAILABLE");
-          //let position2 = t.search("Det finnes ingen ledige eller reserverte dagsverk");
-          if (position1 != -1  )
-            {
-              found=true;
-              //console.log("Found .." + position1.toString() + "  " + position2.toString());
-            }
-
-          //if (found) 
-          //{
-            console.log('FOUND AVAILABLE dutie');
-            numberOfEmails++;
-
-            
-            var transporter = NodeMailer.createTransport({
-              service: 'gmail',
-              auth: {
-                user: 'jannaki69@gmail.com',
-                pass: 'qopo mxog serg xmur'
-              }
-            });
-
-            var mailOptions = {
-              from: 'jannaki69@gmail.com',
-              to: 'jannaki69@hotmail.com',
-              subject: 'Sending Email using Node.js',
-              text: 'That was easy!' + numberOfEmails.toString()
-            };
-
-            transporter.sendMail(mailOptions, function (error, info) {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log('Email sent: ' + info.response);
-                lastEmailTime= Date.now();
-                numberOfEmails++;
-              }
-            });
-          //}
-          //else
-          //{
-           // numberOfEmails = 0;
-          //}  
-          //await browser.close();
-          
-          const d = new Date();
-          var text = d.toDateString() + ' ' + d.toTimeString();
-          
-          console.log(text);
+        var mailOptions = {
+          from: 'jannaki69@gmail.com',
+          to: 'jannaki69@hotmail.com',
+          subject: 'Sending Email using Node.js',
+          text: 'That was easy!' + numberOfEmails.toString()
         };
-      //};
 
-   
-  } catch (e) {
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+              lastEmailTime= Date.now();
+              numberOfEmails++;
+          }
+          });
+      }
+      else
+      {
+        numberOfEmails = 0;
+      };
+        
+      const d = new Date();
+      var text = d.toDateString() + ' ' + d.toTimeString();
+      
+      console.log(text);
+    }
+  } 
+  catch (e) 
+  {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    //await browser.close();
-  }
+  };
 };
 
 async function searchDutiesPeriodically(page){
