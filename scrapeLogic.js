f82af3f4c7a6568885a9c9631f45f6fd6e5790b9
Thalassa <jannaki69@hotmail.com>
2024-03-29 20:57:20 +0100

    added search for duties testing noofemails

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const NodeMailer = require('nodemailer');
 const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
+const { application } = require("express");
 
 require("dotenv").config();
 
@@ -68,6 +69,125 @@ const scrapeLogic = async (res) =>
 };
 
 async function searchDuties(page){
+
+  try {
+    //const page = await browser.newPage();
+    await page.goto(openDutiesText);
+
+      
+      //async () => 
+      //{
+        /*
+        var timeNow = Date.now();
+        var timeDiff = timeNow - lastEmailTime; //in ms
+        // strip the ms
+        timeDiff /= (1000*60);
+
+        // get seconds 
+        var minutesSinceLastEmail = Math.round(timeDiff);
+
+        if (minutesSinceLastEmail > 30)
+        {
+          //reset
+          numberOfEmails = 0;
+          lastEmailTime = Date.now();
+        }
+        
+        if (numberOfEmails > 4)
+        {
+          console.log(minutesSinceLastEmail.toString() + " minutes since last email and no of emails > 4");
+        }
+        else
+        */
+        {
+          
+          await page.reload();
+          console.log("RELOAD done " + page.url().toString());
+
+          //await page.goto('file:///C:/Users/janna/VS/w3school/SISCOG Web App c2 en reservert en ledig.html');
+
+          if ('https://tpowebservice.nsb.no:8501/app/login' == page.url().toString()) 
+          {
+            await loginPage(page);
+          }
+          
+          //page.once('load', () => console.log('Page loaded!'));
+          let found=false;
+          
+          const sleep =(ms=30000) => new Promise(resolve => setTimeout(resolve, ms));
+          await sleep(30000);
+
+          const extractedText = await page.$eval('*', (el) => el.innerText);
+          console.log("INNER TEXT:");
+          console.log(extractedText);
+
+          //const extractedText2 = await page.$eval('*', (el) => el.innerHTML);
+          //console.log("INNER HTML:");
+          //console.log(extractedText2);
+          
+          let t = String(extractedText).toUpperCase();
+          let position1 = t.search("DUTIES AVAILABLE");
+          //let position2 = t.search("Det finnes ingen ledige eller reserverte dagsverk");
+          if (position1 != -1  )
+            {
+              found=true;
+              //console.log("Found .." + position1.toString() + "  " + position2.toString());
+            }
+
+          //if (found) 
+          //{
+            console.log('FOUND AVAILABLE dutie');
+            numberOfEmails++;
+
+            
+            var transporter = NodeMailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: 'jannaki69@gmail.com',
+                pass: 'qopo mxog serg xmur'
+              }
+            });
+
+            var mailOptions = {
+              from: 'jannaki69@gmail.com',
+              to: 'jannaki69@hotmail.com',
+              subject: 'Sending Email using Node.js',
+              text: 'That was easy!' + numberOfEmails.toString()
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('Email sent: ' + info.response);
+                lastEmailTime= Date.now();
+                numberOfEmails++;
+              }
+            });
+          //}
+          //else
+          //{
+           // numberOfEmails = 0;
+          //}  
+          //await browser.close();
+          
+          const d = new Date();
+          var text = d.toDateString() + ' ' + d.toTimeString();
+          
+          console.log(text);
+        };
+      //};
+
+   
+  } catch (e) {
+    console.error(e);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    //await browser.close();
+  }
+};
+
+async function searchDutiesPeriodically(page){
 
   try {
     //const page = await browser.newPage();
